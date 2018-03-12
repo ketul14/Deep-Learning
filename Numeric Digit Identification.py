94232bd4eaf7648f7c9f9da944e1dc8890bddde2
Ketul Patel <ketulpatel1409@gmail.com>
2018-03-11 18:42:44 -0700

    Update Numeric Digit Identification.py

--- Numeric Digit Identification.py
+++ Numeric Digit Identification.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Sun Mar 11 16:56:24 2018
-
 @author: Ketul Patel
-
 A second project to identify the hand written digits using simple Neural Network 
-in Deep Learning
-
+in Deep Learning . The purpose of this is to learn multiclass handwritten numeric digit classification.
 """
 # Import libraries
 from keras.datasets import mnist
